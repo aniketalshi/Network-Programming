7e04ff432d89cdaf944dec5c35a38caa7ba5dd34
Aniket Alshi <aalshi@compserv3.(none)>
2014-10-20 04:30:55 -0400

    client function to match longest prefix ip

--- assign2/client.c
+++ assign2/client.c
@@ -3,39 +3,56 @@
 #include <string.h>
 #include "structs.h"
 
+#define BYTE 8
 extern struct ifi_info *Get_ifi_info_plus(int family, int doaliases);
 extern void free_ifi_info_plus(struct ifi_info *ifihead);
+
+int get_match (unsigned long num1, unsigned long num2){
+    int i, cnt = 0; 
+    for (i = 0; i < sizeof(unsigned long); ++i) {
+	int offset = (3-i)*BYTE;
+	
+	if (((num1 << (i*BYTE)) >> offset) == 0)
+	    break;
+
+	if(~((num1 << (i*BYTE) >> offset)^(num2 << (i*BYTE)>> offset)) == 0xFFFFFFFF) {
+	    cnt++;
+	    continue;
+	}
+	break;
+    }
+    return cnt;
+}
+
 
 void get_client_ip (struct sock_struct *sock_struct_head, struct sockaddr_in *cli_addr, char *srv_ip) {
     struct sockaddr_in srv, *ntmsk;
     struct in_addr subnet;
     struct sock_struct *st = sock_struct_head;
-    int is_loopback = 0;
+    int is_loopback = 0, max_match = 0, match;
     char str[INET_ADDRSTRLEN], str1[INET_ADDRSTRLEN];
 
     inet_pton(AF_INET, srv_ip, &(srv.sin_addr));
     
     while(st) {
-	/* if there is perfect match between (server ip & netmask) 
-	 * and subnet_addr then server is local
-	 */
-	ntmsk = ((struct sockaddr_in *)st->net_mask);
+	ntmsk  = ((struct sockaddr_in *)st->net_mask);
 	subnet = st->subnetaddr;
+	match = get_match((srv.sin_addr.s_addr & ntmsk->sin_addr.s_addr), subnet.s_addr);
+	//printf("match %d\n", match);
 	
-	if ((srv.sin_addr.s_addr & ntmsk->sin_addr.s_addr) == subnet.s_addr) {
-	    cli_addr = ((struct sockaddr_in *)st->ip_addr);
+	if (match > max_match) {
+	    max_match = match; 
+	    cli_addr  = ((struct sockaddr_in *)st->ip_addr);
 	    if(st->is_loopback == 1) {
 		is_loopback = 1;
 	    } 
-	    
-	    inet_ntop(AF_INET, &(cli_addr->sin_addr), str, INET_ADDRSTRLEN); 
-	    inet_ntop(AF_INET, &(srv.sin_addr), str1, INET_ADDRSTRLEN); 
-    	    printf ("\n Server IP: %s \t Ip choosen by client : %s\n", str1, str); 
-	    break;
 	}
-	
 	st = st->nxt_struct;
     }
+	    
+    inet_ntop(AF_INET, &(cli_addr->sin_addr), str, INET_ADDRSTRLEN); 
+    inet_ntop(AF_INET, &(srv.sin_addr), str1, INET_ADDRSTRLEN); 
+    printf ("\n Server IP: %s \t Ip choosen by client : %s\n", str1, str); 
 }
 
 int main(int argc, char* argv[]) {
@@ -72,7 +89,7 @@ int main(int argc, char* argv[]) {
     print_sock_struct (sock_struct_head);
     free_ifi_info_plus(ifihead);
    
-    get_client_ip(sock_struct_head, &cli_addr, "130.245.1.100"); 
+    get_client_ip(sock_struct_head, &cli_addr, "130.245.1.182"); 
 
     return 0;
 }
